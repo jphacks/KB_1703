0a318cdbb53500e28d477d1238ee5c995fd0c1a2
Ian Brison <buo1005@mail4.doshisha.ac.jp>
2017-10-22 02:09:12 +0900

    update image

--- Android/app/src/main/java/haruurara/android/UserUpdateActivity.java
+++ Android/app/src/main/java/haruurara/android/UserUpdateActivity.java
@@ -1,15 +1,26 @@
 package haruurara.android;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class UserUpdateActivity extends AppCompatActivity {
     Globals globals;
+
+    ImageView imageView;
+    int userNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +29,22 @@ public class UserUpdateActivity extends AppCompatActivity {
 
         globals = (Globals)this.getApplication();
 
-        final int userNum = getIntent().getIntExtra("userNum", 0);
+
+        userNum = getIntent().getIntExtra("userNum", 0);
         final EditText name = (EditText) findViewById(R.id.name);
         name.setText(globals.users.get(userNum).name);
+
+        imageView = (ImageView)findViewById(R.id.user_image);
+        imageView.setImageBitmap(globals.users.get(userNum).my_image);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(intent, 42);
+            }
+        });
 
         Button updateButton = (Button) findViewById(R.id.updateButton);
         updateButton.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +69,22 @@ public class UserUpdateActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == 42 && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    imageView.setImageBitmap(bitmap);
+                    globals.users.get(userNum).my_image = bitmap;
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
