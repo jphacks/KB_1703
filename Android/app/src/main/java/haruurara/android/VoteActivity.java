edf9f0af70beef12a5c4c7afa98d4473511e8e32
Ian Brison <buo1005@mail4.doshisha.ac.jp>
2017-10-21 19:53:22 +0900

    add vote list

--- Android/app/src/main/java/haruurara/android/VoteActivity.java
+++ Android/app/src/main/java/haruurara/android/VoteActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
@@ -16,6 +17,7 @@ public class VoteActivity extends AppCompatActivity {
     int user_num;
     int vote_num;
     Globals globals;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,72 +30,41 @@ public class VoteActivity extends AppCompatActivity {
         user_num = intent.getIntExtra("user_num", 0);
         vote_num = -1;
 
-        Spinner vote_spinner = (Spinner)findViewById(R.id.vote_spinner);
+        listView = (ListView)findViewById(R.id.vote_list_view);
+        VoteAdapter voteAdapter = new VoteAdapter(this, R.layout.vote_list, globals.users, user_num);
+        listView.setAdapter(voteAdapter);
 
-        String[] user_nums = new String[globals.CountAliveUser() - 1];
-        int tmp_num = 0;
-        for (int i = 0; i < globals.CountAliveUser() - 1;) {
-            if(tmp_num != user_num && globals.users.get(tmp_num).is_alive){
-                user_nums[i] = Integer.toString(tmp_num);
-                i++;
+    }
+
+    public void vote(int selected_vote_num){
+        int next_alive_user_num = -1;
+        for(int i = user_num + 1; i < globals.users.size(); i++){
+            if(globals.users.get(i).is_alive){
+                next_alive_user_num = i;
+                break;
             }
-            tmp_num++;
         }
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, user_nums);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        vote_spinner.setAdapter(arrayAdapter);
-
-        vote_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Spinner spinner = (Spinner)parent;
-                String item = (String) spinner.getSelectedItem();
-                vote_num = Integer.parseInt(item);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        Button Vote_ok_button = (Button)findViewById(R.id.Vote_ok_button);
-        Vote_ok_button.setText(Integer.toString(user_num));
-        Vote_ok_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int next_alive_user_num = -1;
-                for(int i = user_num + 1; i < globals.users.size(); i++){
-                    if(globals.users.get(i).is_alive){
-                        next_alive_user_num = i;
-                        break;
-                    }
-                }
-                globals.votes[vote_num]++;
-                if(next_alive_user_num != -1) {
-                    Intent intent = new Intent();
-                    intent.setClassName("haruurara.android", "haruurara.android.VoteActivity");
-                    intent.putExtra("user_num", next_alive_user_num);
-                    startActivity(intent);
-                    VoteActivity.this.finish();
-                } else {
-                    int kill_user_num = -1;
-                    int kill_vote_num = 0;
-                    for (int i = 0; i < globals.votes.length; i++){
-                        if(globals.votes[i] > kill_vote_num){
-                            kill_vote_num = globals.votes[i];
-                            kill_user_num = i;
-                        }
-                    }
-                    Intent intent = new Intent();
-                    intent.setClassName("haruurara.android", "haruurara.android.KillActivity");
-                    intent.putExtra("kill_user_num", kill_user_num);
-                    startActivity(intent);
-                    VoteActivity.this.finish();
+        globals.votes[selected_vote_num]++;
+        if(next_alive_user_num != -1) {
+            Intent intent = new Intent();
+            intent.setClassName("haruurara.android", "haruurara.android.VoteActivity");
+            intent.putExtra("user_num", next_alive_user_num);
+            startActivity(intent);
+            VoteActivity.this.finish();
+        } else {
+            int kill_user_num = -1;
+            int kill_vote_num = 0;
+            for (int i = 0; i < globals.votes.length; i++){
+                if(globals.votes[i] > kill_vote_num){
+                    kill_vote_num = globals.votes[i];
+                    kill_user_num = i;
                 }
             }
-        });
-
-
+            Intent intent = new Intent();
+            intent.setClassName("haruurara.android", "haruurara.android.KillActivity");
+            intent.putExtra("kill_user_num", kill_user_num);
+            startActivity(intent);
+            VoteActivity.this.finish();
+        }
     }
 }
---
+++ Android/app/src/main/java/haruurara/android/VoteAdapter.java
@@ -0,0 +1,57 @@
+package haruurara.android;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ian on 2017/10/21.
+ */
+
+public class VoteAdapter extends ArrayAdapter<User>{
+
+    private LayoutInflater layoutInflater;
+    VoteActivity voteActivity;
+    int user_num;
+
+    public VoteAdapter(Context context, int resource, List<User> objects, int user_num) {
+        super(context, resource, objects);
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        voteActivity = (VoteActivity)this.getContext();
+        this.user_num = user_num;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent){
+        User item = (User)getItem(position);
+
+        if(null == convertView){
+            convertView = layoutInflater.inflate(R.layout.vote_list, null);
+        }
+
+        TextView textView = (TextView)convertView.findViewById(R.id.vote_name);
+        textView.setText(item.name);
+
+        Button button = (Button)convertView.findViewById(R.id.vote_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                voteActivity.vote(position);
+            }
+        });
+        if (position == user_num){
+            textView.setVisibility(View.GONE);
+            button.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+}
