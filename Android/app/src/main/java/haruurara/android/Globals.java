7a36726d222f04da78ae6d6cf164ec6267e1396b
Ian Brison <buo1005@mail4.doshisha.ac.jp>
2017-10-21 13:22:19 +0900

    add global and user class

---
+++ Android/app/src/main/java/haruurara/android/Globals.java
@@ -0,0 +1,14 @@
+package haruurara.android;
+
+import android.app.Application;
+
+/**
+ * Created by ian on 2017/10/21.
+ */
+
+public class Globals extends Application {
+
+    User users[];
+    int time;
+
+}
