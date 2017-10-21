95c08ffbb7888904d2715e7d0b9948a7bb1497ff
PiroHiroPiro <hirohiro11071107@gmail.com>
2017-10-22 01:33:13 +0900

    can send data to firebase

--- Android/app/src/main/java/haruurara/android/StartActivity.java
+++ Android/app/src/main/java/haruurara/android/StartActivity.java
@@ -19,7 +19,7 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.setClassName("haruurara.android", "haruurara.android.WriteIdeaActivity");
+                intent.setClassName("haruurara.android", "haruurara.android.UserResisterActivity");
                 startActivity(intent);
                 StartActivity.this.finish();
             }
